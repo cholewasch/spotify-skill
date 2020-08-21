ce8dc367ab765361d577f2a1cd5a070d015bb50d
cholewasch <69981977+cholewasch@users.noreply.github.com>
2020-08-21 11:32:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from mycroft import MycroftSkill, intent_file_handler
 class Spotify(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.learning = True
 
     @intent_file_handler('spotify.intent')
     def handle_spotify(self, message):
